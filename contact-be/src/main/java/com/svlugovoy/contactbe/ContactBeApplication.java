0815e30bfa0f3d58d2b79c3fe3fd62d495447085
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-06-29 18:17:47 +0300

    Started development

---
+++ contact-be/src/main/java/com/svlugovoy/contactbe/ContactBeApplication.java
@@ -0,0 +1,13 @@
+package com.svlugovoy.contactbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactBeApplication.class, args);
+    }
+
+}
